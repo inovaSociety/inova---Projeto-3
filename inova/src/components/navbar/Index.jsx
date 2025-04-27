c5dbb837dfd9843b48f3f0197674fd881c43815e
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-04-27 05:38:14 -0300

    update no componente da navbar

--- inova/src/components/navbar/Index.jsx
+++ inova/src/components/navbar/Index.jsx
@@ -6,19 +6,21 @@ const Index = () => {
   return (
     <>
         <div className='header'>
+            <div className='container'>
             <div className='logo'>
                 <img src={logo} alt="logo do site " />
             </div>
             <div className='right'>
-                <a href=""> Home </a>
-                <a href=""> Projetos</a>
-                <a href=""> Serviços</a>
+                <a href="">Home</a>
+                <a href="">Projetos</a>
+                <a href="">Serviços</a>
                 <a href="">Sobre Nós</a>
                 <a href="">Contato</a>
             </div>
             <div className='b-teste'>
                 <a href="">Entre em contato</a>
-            </div>      
+            </div>
+            </div>    
         </div>
     </>
   )
