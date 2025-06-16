1482dc9a4b51a133c38d1a7d1dd0aab00d81b244
joao miguel <miguel.santos@dcx.ufpb.br>
2025-06-16 18:04:15 -0300

    update about

--- inova/src/components/about/Team.jsx
+++ inova/src/components/about/Team.jsx
@@ -1,56 +1,59 @@
 import React from 'react'
 import Ranger from '../../images/power ranger vermelho.jpg'
 import './Style.css'
+import SetaDireita from '../../images/seta-direita.png'
+import SetaEsquerda from '../../images/seta-esquerda.png'
 
 const Team = () => {
   return (
     <div className='body-team'>
         <div className='wrapper'>
+            <div className='i'>
+                <img src={SetaEsquerda} alt="seta esquerda" width={15}/>
+            </div>
+            
             <ul className='carousel'>
                 <li className='card'>
                     <div className='img'>
                         <img src={Ranger} alt="imagem do profissional" />
+                    </div>
                         <h2>Jason Lee</h2>
                         <span>Desenvolvedor</span>
-                    </div>
-                </li>
-                 <li className='card'>
-                    <div className='img'>
-                        <img src={Ranger} alt="imagem do profissional" />
-                        <h2>Jason Lee</h2>
-                        <span>Desenvolvedor</span>
-                    </div>
                 </li>
                 <li className='card'>
                     <div className='img'>
                         <img src={Ranger} alt="imagem do profissional" />
+                    </div>
                         <h2>Jason Lee</h2>
                         <span>Desenvolvedor</span>
-                    </div>
                 </li>
                 <li className='card'>
                     <div className='img'>
                         <img src={Ranger} alt="imagem do profissional" />
+                    </div>
                         <h2>Jason Lee</h2>
                         <span>Desenvolvedor</span>
-                    </div>
                 </li>
                 <li className='card'>
                     <div className='img'>
                         <img src={Ranger} alt="imagem do profissional" />
+                    </div>
                         <h2>Jason Lee</h2>
                         <span>Desenvolvedor</span>
-                    </div>
                 </li>
                 <li className='card'>
                     <div className='img'>
                         <img src={Ranger} alt="imagem do profissional" />
+                    </div>
                         <h2>Jason Lee</h2>
                         <span>Desenvolvedor</span>
-                    </div>
                 </li>
-                
+                                
             </ul>
+            <div className='i'>
+                <img src={SetaDireita} alt="seta direita" width={15}  />
+            </div>
+            
         </div>
     </div>
     
