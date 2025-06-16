5094e9814f8383058debaa4732a3cf82a321ddda
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-06-16 12:48:38 -0300

    Implementando o componente Team
    Criada uma nova Li apenas para estilização

--- inova/src/components/about/Team.jsx
+++ inova/src/components/about/Team.jsx
@@ -4,19 +4,27 @@ import './Style.css'
 
 const Team = () => {
   return (
-    <div className='wrapper'>
-        <ul className='carousel'>
-            <li className='card'>
-                <div>
-                    <img src={Ranger} alt="imagem do profissional" />
-                    <h2>Jason Lee</h2>
-                    <span>Desenvolvedor</span>
-                </div>
-            </li>
-        </ul>
-
-
+    <div className='body-team'>
+        <div className='wrapper'>
+            <ul className='carousel'>
+                <li className='card'>
+                    <div className='img'>
+                        <img src={Ranger} alt="imagem do profissional" />
+                        <h2>Jason Lee</h2>
+                        <span>Desenvolvedor</span>
+                    </div>
+                </li>
+                 <li className='card'>
+                    <div className='img'>
+                        <img src={Ranger} alt="imagem do profissional" />
+                        <h2>Jason Lee</h2>
+                        <span>Desenvolvedor</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
+    
   )
 }
 
