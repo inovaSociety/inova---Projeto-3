f34d21ecddfe0f5a4b2d236723f461375f128e2d
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-06-16 01:15:45 -0300

    Implementando a pagina Projects

--- inova/src/components/projects/Project.jsx
+++ inova/src/components/projects/Project.jsx
@@ -1,17 +1,61 @@
 import React from 'react'
 import './Style.css'
 import Card from './Card'
+import Pizaaria from "../../images/projeto-pizzaria.jpg"
+
+const projects = [
+    {
+        id: 1,
+        title: "Pizzaria Landing Page",
+        description: "Um bela landing page de uma pizzaria",
+        image: Pizaaria,
+        tags: ["React", "TailwinndCSS"],
+        demoUrl: "#",
+        gitHubUrl:"#"
+    }
+]
 
 
 const Project = () => {
   return (
-    <div>
-        <h1>CONHEÇA NOSSOS PROJETOS</h1>
-        <br />
-        <br />
+    <section>
+        <div className='projeto-zero'>
+        <h2 className='text-primary'>Futuros <span className='text-secondary'>Projetos</span> </h2>
+        <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>Conheça alguns dos nossos projetos mais recentes, desenvolvidos com foco em inovação, usabilidade e performance. Cada solução reflete nosso compromisso com a excelência em design, tecnologia e experiência do usuário.</p>
+
+        <div className='class-projects-cards'>
+
+            {projects.map((project,key) => (
+                <div
+                    key={key}
+                    className='project-card-um'
+                >
+                    <div className='master-class'>
+                        <img src={project.image} alt={project.title} className='project-image'/>
+                    </div>
+                    <div className='p-6'>
+                        <div className='tags-container'> 
+                            {project.tags.map((tag) =>(
+                                <span className='project-tag'>
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+
+                    <h3 className='project-title'>{project.title}</h3>
+                    <p className='project-description'>{project.description}</p>
+                </div>
+            ))}
+
+
+        </div>
 
         <Card/>
     </div>
+
+    </section>
+    
   )
 }
 
