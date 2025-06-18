e331845f2d23430acfd1c1f1f990e1b7bbebfffc
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-06-18 18:11:17 -0300

    - fix: correção da instalção do react-slcik

--- inova/src/components/about/Team.jsx
+++ inova/src/components/about/Team.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Ranger from '../../images/power ranger vermelho.jpg'
+
+
 import './Style.css'
 import SetaDireita from '../../images/seta-direita.png'
 import SetaEsquerda from '../../images/seta-esquerda.png'
 
 const Team = () => {
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll:1
+    }
 
     const data = [
         {
@@ -12,11 +25,25 @@ const Team = () => {
             name:"Jason Lee",
             img:Ranger,
             job:"Desenvolvedor"
+        },
+        {
+            id: 2,
+            name:"Jason Lee",
+            img:Ranger,
+            job:"Desenvolvedor"
+        },
+        {
+            id: 3,
+            name:"Jason Lee",
+            img:Ranger,
+            job:"Desenvolvedor"
+        },
+        {
+            id: 4,
+            name:"Jason Lee",
+            img:Ranger,
+            job:"Desenvolvedor"
         }
-
-
-
-
 
     ]
   return (
@@ -24,26 +51,23 @@ const Team = () => {
         <div className='wrapper'>
 
 
-            {/*<div className='i'>
-                <img src={SetaEsquerda} alt="seta esquerda" width={15}/>
-            </div>*/}
+           
+            <Slider {...settings}>
+                {data.map((d) =>(
+                    <ul className='carousel'>
+                        <li className='card'>
+                            <div className='img'>
+                                <img src={d.img} alt="imagem do profissional" />
+                            </div>
+                                <h2>{d.name}</h2>
+                                <span>{d.job}</span>
+                        </li>           
+                    </ul>
+                ))}
+            </Slider>
+            
 
-            {data.map((d) =>(
-                
-                <ul className='carousel'>
-                    <li className='card'>
-                        <div className='img'>
-                            <img src={d.img} alt="imagem do profissional" />
-                        </div>
-                            <h2>{d.name}</h2>
-                            <span>{d.job}</span>
-                    </li>           
-                </ul>
-            ))}
-
-            {/*<div className='i'>
-                <img src={SetaDireita} alt="seta direita" width={15}  />
-            </div>*/}
+           
             
         </div>
     </div>
