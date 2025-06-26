3ec83df200c8db207b61823659f2c4242ecd882f
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-06-25 21:30:04 -0300

    Criando um novo componente

--- inova/src/components/about/About.jsx
+++ inova/src/components/about/About.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import Team from './Team'
 import './Style.css'
+import Faixa from './Faixa'
 
 
 const About = () => {
   return (
     <div>
         <h1>Sobre nós</h1>
-        <div className='faixa-prom'>
-          <h1> Vamos trabalhar juntos </h1>
-          <h4> Entre em contato com nossos especialistas para analisar sua <br />
-            ideia ou produto e discutir opções para a melhor abordagem</h4>
-            <a href="" className='b-teste'>Entre em contato</a>
-        </div>
+        <Faixa/>
         <Team/>
     </div>
   )
---
+++ inova/src/components/about/Faixa.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Style.css'
+
+
+const Faixa = () => {
+  return (
+
+        <div className='faixa-prom'>
+            <h1> Vamos trabalhar juntos </h1>
+            <h4> Entre em contato com nossos especialistas para analisar sua <br />
+                ideia ou produto e discutir opções para a melhor abordagem</h4>
+                <a href="" className='b-teste'>Entre em contato</a>
+        </div>
+    
+  )
+}
+
+export default Faixa
