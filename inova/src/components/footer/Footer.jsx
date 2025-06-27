af07e89a59de7782d856b3f01be68934c6fbd05f
José Rian <140354064+Drianrito@users.noreply.github.com>
2025-06-27 18:24:49 -0300

    Update footer

--- inova/src/components/footer/Footer.jsx
+++ inova/src/components/footer/Footer.jsx
@@ -1,55 +1,61 @@
-import './Style.css'
-import React from 'react'
-import logo from '../../images/logo.svg'
-import instagram from '../../images/instagram.png'
-import linkedin from '../../images/linkedin.png'
-import github from '../../images/github.png'
+import './Style.css';
+import React from 'react';
+import logo from '../../images/logo.svg';
+import instagram from '../../images/instagram.png';
+import linkedin from '../../images/linkedin.png';
+import github from '../../images/github.png';
+
 const Footer = () => {
   return (
-    <>
-      <div className='footer'>
+    <div className='footer'>
+      <div className='footer-content'>
         <div className='inova-column'>
-          <h3><img src={logo} alt="logo do site" /></h3>
-          <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ad consequuntur magnam rerum doloribus itaque aliquid culpa, eos minima! Dolor at cum odit deserunt? Molestiae in ratione culpa qui recusandae.</h4>
+          <div className='logo-footer'>
+            <img src={logo} alt="logo do site" />
+          </div>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta delectus deleniti
+            doloremque ipsam, repudiandae molestiae temporibus dolor necessitatibus minima aliquid
+            praesentium, facere, explicabo ipsum nesciunt pariatur laborum quos in facilis.
+          </h4>
           <ul className='social-icons'>
-            <li><img src={instagram} alt="logo Instagram " /></li>
-            <li><img src={linkedin} alt="logo linkedin " /></li>
-            <li><img src={github} alt="logo github " /></li>
+            <li><img src={instagram} alt="logo Instagram" /></li>
+            <li><img src={linkedin} alt="logo LinkedIn" /></li>
+            <li><img src={github} alt="logo GitHub" /></li>
           </ul>
         </div>
+
         <div className='services-column'>
-          <div className='footer-services'>
-            <h3>Serviços</h3>
-            <ul>
-              <li><a href="#">Desenvolvimento Web</a></li>
-              <li><a href="#">Ux/UI Design</a></li>
-              <li><a href="#">Modelagem de Negocio</a></li>
-              <li><a href="#">Desenvolvimento Mobile</a></li>
-            </ul>
-          </div>
-        </div>
-        <div className='about-column'>
-          <div className='footer-about'>
-            <h3>Informações</h3>
-            <ul>
-              <li><a href="#">Perguntas Frequentes</a></li>
-              <li><a href="#">Processo de Trabalho</a></li>
-              <li><a href="#">Sobre nos</a></li>
-              <li><a href="#">Termos e condicoes</a></li>
-            </ul>
-          </div>
-        </div>
-        <div className='news-column'>
-          <h3>Newsletter</h3>
-          <p>Inscreva-se para receber novidades e atualizações.</p>
-          <form>
-            <input type="email" placeholder="Seu email" required />
-            <button type="submit">Inscrever-se</button>
-          </form>
+          <h3>Serviços</h3>
+          <ul className='footer-list'>
+            <li><a href="#">Desenvolvimento Web</a></li>
+            <li><a href="#">UX/UI Design</a></li>
+            <li><a href="#">Modelagem de Negocio</a></li>
+            <li><a href="#">Desenvolvimento Mobile</a></li>
+          </ul>
         </div>
 
+        <div className='about-column'>
+          <h3>Informações</h3>
+          <ul className='footer-list'>
+            <li><a href="#">Perguntas Frequentes</a></li>
+            <li><a href="#">Processo de Trabalho</a></li>
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Termos e condições</a></li>
+          </ul>
+        </div>
+
+        <div className='contact-column'>
+          <h3>Entrar em contato</h3>
+          <p>Digite seu email para contato</p>
+          <form>
+            <input type="email" placeholder="Seu email" required />
+            <button type="submit">Entrar em contato</button>
+          </form>
+        </div>
       </div>
-    </>
-  )
-}
-export default Footer
+    </div>
+  );
+};
+
+export default Footer;
