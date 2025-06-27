5687b04eca06c72cde180a131e394671b13a1e3a
José Rian <140354064+Drianrito@users.noreply.github.com>
2025-06-27 13:25:23 -0300

    Add social logo image to project

--- inova/src/App.jsx
+++ inova/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home/Home'
 import Service from './components/servicesLp/ServicesLp'
 import Project from './components/projects/Project'
 import About from './components/about/About'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -16,9 +17,8 @@ function App() {
      <Service/>
      <Project/>
     
-     <About/>
-  
-     
+     <About/>  
+     <Footer/>
     </>
   )
 }
--- inova/src/components/footer/Footer.jsx
+++ inova/src/components/footer/Footer.jsx
@@ -1,11 +1,55 @@
-import Style from './Style.css'
+import './Style.css'
 import React from 'react'
+import logo from '../../images/logo.svg'
+import instagram from '../../images/instagram.png'
+import linkedin from '../../images/linkedin.png'
+import github from '../../images/github.png'
 const Footer = () => {
   return (
     <>
-        <div className='footer'>
-            
+      <div className='footer'>
+        <div className='inova-column'>
+          <h3><img src={logo} alt="logo do site" /></h3>
+          <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi ad consequuntur magnam rerum doloribus itaque aliquid culpa, eos minima! Dolor at cum odit deserunt? Molestiae in ratione culpa qui recusandae.</h4>
+          <ul className='social-icons'>
+            <li><img src={instagram} alt="logo Instagram " /></li>
+            <li><img src={linkedin} alt="logo linkedin " /></li>
+            <li><img src={github} alt="logo github " /></li>
+          </ul>
         </div>
+        <div className='services-column'>
+          <div className='footer-services'>
+            <h3>Serviços</h3>
+            <ul>
+              <li><a href="#">Desenvolvimento Web</a></li>
+              <li><a href="#">Ux/UI Design</a></li>
+              <li><a href="#">Modelagem de Negocio</a></li>
+              <li><a href="#">Desenvolvimento Mobile</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='about-column'>
+          <div className='footer-about'>
+            <h3>Informações</h3>
+            <ul>
+              <li><a href="#">Perguntas Frequentes</a></li>
+              <li><a href="#">Processo de Trabalho</a></li>
+              <li><a href="#">Sobre nos</a></li>
+              <li><a href="#">Termos e condicoes</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className='news-column'>
+          <h3>Newsletter</h3>
+          <p>Inscreva-se para receber novidades e atualizações.</p>
+          <form>
+            <input type="email" placeholder="Seu email" required />
+            <button type="submit">Inscrever-se</button>
+          </form>
+        </div>
+
+      </div>
     </>
-    )
+  )
 }
+export default Footer
