93683443921021a05e0920d74e170a9cab67eb24
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-06-28 04:46:55 -0300

    Atualiza  o Home.jsx e seu  CSS

--- inova/src/components/home/Home.jsx
+++ inova/src/components/home/Home.jsx
@@ -1,25 +1,30 @@
-import './Style.css'
-import React from 'react'
-import equipe from '../../images/pexels-mart-production-7550298.jpg'
+import "./Style.css";
+import React from "react";
+import equipe from "../../images/pexels-mart-production-7550298.jpg";
 const Home = () => {
   return (
     <>
-        <div className='hero'>
-            <div className='h-left'>
-                <div className='title'>
-                    <div className='line'>Inovação e usabilidade <br></br> em cada detalhe <br></br> Destaque sua empresa online</div>
-                </div>
-                <div className='links'>
-                    <a href="" className='b-teste'>Conheça os projetos</a>
-                </div>
+      <div className="hero">
+        <div className="h-left">
+          <div className="title">
+            <div className="line">
+              <span className="sparkle">INOVAÇÃO</span> <span className="line-one">  e usabilidade</span>
+              <span className="line-two"> em cada detalhe</span>
+              <span className="sparkle">DESTAQUE</span> <span className="line-three">  sua empresa online</span>
             </div>
-            <div className='h-right'>
-                <img src={equipe} alt="side" width={350} className='n-img' />
-            </div>
-
+          </div>
+          <div className="links">
+            <a href="" className="b-teste">
+              Conheça os projetos
+            </a>
+          </div>
         </div>
+        <div className="h-right">
+          <img src={equipe} alt="side" width={350} className="n-img" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
