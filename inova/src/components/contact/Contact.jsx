9debef89e907ae3bf277dbd5082b2d4b8a1ab50c
José Rian <140354064+Drianrito@users.noreply.github.com>
2025-07-02 17:58:53 -0300

    Add contact form component with styling

--- inova/src/components/contact/Contact.jsx
+++ inova/src/components/contact/Contact.jsx
@@ -8,8 +8,39 @@ const Contact = () => {
     <div id='Contact'>
       <Index />
       <div>
-        Contact
-        <h1>Teste</h1>
+        <form className='contact-form'>
+          <h1 className='titulo-contato'>Fale Conosco!</h1>
+          <div className='form-group'>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor="phone">Telefone:</label>
+            <input type="tel" id="phone" name="phone" required />
+          </div>
+          <div className='form-group'>
+            <label for="interest">Assunto desejado:</label>
+            <select id="interest" name="interest" required>
+              <option value="" disabled selected hidden>Selecione uma opção</option>
+              <option value="programa">Desenvolvimento Web</option>
+              <option value="ux/ui">UX/UI Design</option>
+              <option value="modelagem">Modelagem de Negocio</option>
+            </select>
+          </div>
+          <div className='form-group'>
+            <label htmlFor="message">Mensagem:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+          </div>
+
+          <button type="submit">Enviar</button>
+        </form>
+
       </div>
     </div>
   )
