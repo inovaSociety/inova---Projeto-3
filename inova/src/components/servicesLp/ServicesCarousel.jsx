7ded58cc820f0cb703bdf5d293f52464ae91bab7
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2025-09-07 14:57:04 -0300

    update servicesCarousel

--- inova/src/components/servicesLp/ServicesCarousel.jsx
+++ inova/src/components/servicesLp/ServicesCarousel.jsx
@@ -2,6 +2,44 @@ import React from "react";
 import Slider from "react-slick";
 import "./ServicesCarousel.css";
 
+// seta esquerda
+const PrevArrow = ({ onClick }) => (
+  <button className="custom-arrow prev" onClick={onClick}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#3037BF"      // contorno branco
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="15 18 9 12 15 6" />
+    </svg>
+  </button>
+);
+
+// seta direita
+const NextArrow = ({ onClick }) => (
+  <button className="custom-arrow next" onClick={onClick}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#3037BF"      // contorno branco
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  </button>
+);
+
 const ServicesCarousel = () => {
   const settings = {
     dots: false,
@@ -10,6 +48,8 @@ const ServicesCarousel = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
     appendArrows: (arrows) => (
       <div className="custom-arrows">{arrows}</div>
     ),
@@ -24,7 +64,6 @@ const ServicesCarousel = () => {
 
   return (
     <div className="services-carousel">
-
       <Slider {...settings}>
         {services.map((service, index) => (
           <div key={index} className="service-card">
